6b0f4d7530f410ebe788d70ee381decb8551f7de
hsinlunlun <hsinlun.chiu@gmail.com>
2022-03-26 14:46:41 +0800

    Layout 0.1 version finish

--- src/Layout.java
+++ src/Layout.java
@@ -2,86 +2,71 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Layout {
+    protected JFrame frame = new JFrame("UML Editor");
+
+
     public Layout(){
-        JFrame frame = new JFrame("UML Editor");
-//        frame.setSize(1024, 600);
+        frame.setSize(1024, 600);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel(new GridBagLayout());
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+//        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
         frame.setContentPane(panel);
 
         // cstr for GridBagLayout
         GridBagConstraints cstr = new GridBagConstraints();
-        cstr.weightx = 1;
-        cstr.weighty = 1;
         cstr.fill = GridBagConstraints.BOTH;
-
-        // button(mode) list
-        JPanel paneModeList = new JPanel(new GridBagLayout());
-        cstr.gridx = 0;
-        cstr.gridy = 0;
-        cstr.gridwidth = 1;
-        cstr.gridheight = 6;
-        panel.add(paneModeList, cstr);
-
-        GridBagConstraints cstrModeBtn = new GridBagConstraints();
-        cstrModeBtn.weightx = 1;
-        cstrModeBtn.weighty = 1;
-        cstrModeBtn.fill = GridBagConstraints.BOTH;
+        cstr.insets = new Insets(5, 5, 5, 5);
 
         JButton btnSelect = new JButton("Select");
-        cstrModeBtn.gridx = 0;
-        cstrModeBtn.gridy = 0;
-        paneModeList.add(btnSelect, cstrModeBtn);
+        setLyoutCstr(cstr, 0, 0, 1, 1, 0, 1);
+        panel.add(btnSelect, cstr);
 
 
         JButton btnAssocLine = new JButton("Association Line");
-        cstrModeBtn.gridx = 0;
-        cstrModeBtn.gridy = 1;
-        paneModeList.add(btnAssocLine, cstrModeBtn);
+        setLyoutCstr(cstr, 0, 1, 1, 1, 0, 1);
+        panel.add(btnAssocLine, cstr);
 
 
         JButton btnGeneLine = new JButton("Generalization Line");
-        cstrModeBtn.gridx = 0;
-        cstrModeBtn.gridy = 2;
-        paneModeList.add(btnGeneLine, cstrModeBtn);
+        setLyoutCstr(cstr, 0, 2, 1, 1, 0, 1);
+        panel.add(btnGeneLine, cstr);
 
 
         JButton btnCompLine = new JButton("Composition Line");
-        cstrModeBtn.gridx = 0;
-        cstrModeBtn.gridy = 3;
-        paneModeList.add(btnCompLine, cstrModeBtn);
+        setLyoutCstr(cstr, 0, 3, 1, 1, 0, 1);
+        panel.add(btnCompLine, cstr);
 
 
         JButton btnClass = new JButton("Class");
-        cstrModeBtn.gridx = 0;
-        cstrModeBtn.gridy = 4;
-        paneModeList.add(btnClass, cstrModeBtn);
+        setLyoutCstr(cstr, 0, 4, 1, 1, 0, 1);
+        panel.add(btnClass, cstr);
+
+
 
 
         JButton btnUseCase = new JButton("Use Case");
-        cstrModeBtn.gridx = 0;
-        cstrModeBtn.gridy = 5;
-        paneModeList.add(btnUseCase, cstrModeBtn);
+        setLyoutCstr(cstr, 0, 5, 1, 1, 0, 1);
+        panel.add(btnUseCase, cstr);
 
 
         // canvas panel
-        GridBagConstraints cstrCanvas = new GridBagConstraints();
         JPanel paneCanvas = new JPanel();
         paneCanvas.setBackground(Color.white);
-        cstrCanvas.gridx = 1;
-        cstrCanvas.gridy = 0;
-        cstrCanvas.gridwidth = 6;
-        cstrCanvas.gridheight = 6;
-        cstrCanvas.weightx = 1;
-        cstrCanvas.weighty = 1;
-        cstrCanvas.fill = GridBagConstraints.BOTH;
-        cstrCanvas.anchor = GridBagConstraints.WEST;
+        setLyoutCstr(cstr, 1, 0, 6, 6, 1, 1);
+        panel.add(paneCanvas, cstr);
 
-        panel.add(paneCanvas, cstrCanvas);
-
-        frame.pack(); // Adjust the frame's size to fit the inner components.
+//        frame.pack(); // Adjust the frame's size to fit the inner components.
         frame.setVisible(true);
     }
+
+    public void setLyoutCstr(GridBagConstraints _cstr, int _gridx, int _gridy, int _gridwidth, int _gridheight, int _weightx, int _weighty){
+        _cstr.gridx = _gridx;
+        _cstr.gridy = _gridy;
+        _cstr.gridwidth = _gridwidth;
+        _cstr.gridheight = _gridwidth;
+        _cstr.weightx = _weightx;
+        _cstr.weighty = _weighty;
+    }
+
 }
